f0220e4ee1816599188bbb80b9116a7e8a4e9aa0
WangQiRui <wangqr@haoyitec.cn>
2023-03-17 17:26:31 +0800

    push

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-npm run build
+npm run deploy
 
-cd public
-
-git init
-git add -A
-git commit -m 'deploy'
-
-git push -f git@github.com:ArikarWang/arikarblog.git master:gh-pages
-# git push -f git@gitee.com:arikar/arikarblog.git master:gh-pages
-
-cd ../
 rm -rf public
